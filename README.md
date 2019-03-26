246b148186d699ec1b0dfd35e9ba86021367f71d
Bruce Becker <brucellino@gmail.com>
2019-03-27 00:52:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Base Platform
+
+This role provides the base platform for my laptop.
+
+## Requirements
+
+
+Just a working OS and a sudo enabled user
+
+## Role Variables
+
+- packages
+
+## Dependencies
+
+None
+
+## Example Playbook
+
+```
+- hosts: localhost
+  roles:
+      - { role: base-platform }
+```
+
+
+## License
+
+None
+
+## Author Information
+
+[@brucellino](https://github.com/brucellino)
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,22 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_docker(host):
+    pkg = host.package('docker')
+    assert pkg.is_installed
+    docker_service = host.service('docker')
+    assert docker_service.is_running
+    assert docker_service.is_enabled
